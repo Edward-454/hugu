cd01d9dcbfd14bf5c702735c4b2476468b43ee9f
Edward-454 <xu8901@gmail.com>
2026-02-19 18:46:29 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i 's/192.168.1.1/192.168.110.249/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.110.249/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/redmek40/g' package/base-files/files/bin/config_generate
 # Remove packages
 rm -rf feeds/luci/applications/luci-app-momo
@@ -68,25 +68,3 @@ rm -rf package/small-package/python-zope-event
 rm -rf package/small-package/onionshare-cli
 rm -rf package/small-package/luci-app-wechatpush
 rm -rf package/small-package/exim
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-echo "Lowering Go version requirement from 1.25 to 1.24..."
-# 针对 feeds 目录
-if [ -d "feeds" ]; then
-    find feeds/ -type f -name "go.mod" -exec sed -i 's/go 1.25/go 1.24/g' {} \;
-fi
-
-# 针对 package 目录
-if [ -d "package" ]; then
-    find package/ -type f -name "go.mod" -exec sed -i 's/go 1.25/go 1.24/g' {} \;
-    find package/ -type f -name "Makefile" -exec sed -i 's/GO_VERSION_MIN:=1.25/GO_VERSION_MIN:=1.24/g' {} \;
-fi
-
-echo "Go version downgrade completed."
-# 修正后的 go.mod 修改命令
-find ./ -type f -name "go.mod" -exec sed -i 's/go 1.25/go 1.24/g' {} \;
-
-# 修正后的 Makefile 修改命令
-find ./package/ -type f -name "Makefile" -exec sed -i 's/GO_VERSION_MIN:=1.25/GO_VERSION_MIN:=1.24/g' {} \;
-# 设置默认密码为 password
-#sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' openwrt/package/base-files/files/etc/shadow
-# 修改默认 IP 为 192.168.1.99
