702443d299abe3fe34292ee59537aaae33d66c0e
Edward-454 <xu8901@gmail.com>
2026-02-19 23:44:44 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -68,3 +68,25 @@ rm -rf package/small-package/python-zope-event
 rm -rf package/small-package/onionshare-cli
 rm -rf package/small-package/luci-app-wechatpush
 rm -rf package/small-package/exim
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+echo "Lowering Go version requirement from 1.25 to 1.24..."
+# 针对 feeds 目录
+if [ -d "feeds" ]; then
+    find feeds/ -type f -name "go.mod" -exec sed -i 's/go 1.25/go 1.24/g' {} \;
+fi
+
+# 针对 package 目录
+if [ -d "package" ]; then
+    find package/ -type f -name "go.mod" -exec sed -i 's/go 1.25/go 1.24/g' {} \;
+    find package/ -type f -name "Makefile" -exec sed -i 's/GO_VERSION_MIN:=1.25/GO_VERSION_MIN:=1.24/g' {} \;
+fi
+
+echo "Go version downgrade completed."
+# 修正后的 go.mod 修改命令
+find ./ -type f -name "go.mod" -exec sed -i 's/go 1.25/go 1.24/g' {} \;
+
+# 修正后的 Makefile 修改命令
+find ./package/ -type f -name "Makefile" -exec sed -i 's/GO_VERSION_MIN:=1.25/GO_VERSION_MIN:=1.24/g' {} \;
+# 设置默认密码为 password
+#sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' openwrt/package/base-files/files/etc/shadow
+# 修改默认 IP 为 192.168.1.99
