4ffe4968996f5c08e5be8619170c447b7673ae6c
Edward-454 <xu8901@gmail.com>
2026-08-19 22:15:55 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -15,13 +15,13 @@ git clone https://github.com/dsddr02/cdnspeedtest package/cdnspeedtest
 git clone https://github.com/dsddr02/peed11 package/luci-app-cloudflarespeedtest
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo package/momo
-git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2 --depth=1 package/luci-app-passwall2
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall --depth=1 luci-app-passwall
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall2 --depth=1 package/luci-app-passwall2
 git clone https://github.com/sbwml/luci-app-openlist2 --depth=1 package/luci-app-openlist2
 # git clone https://github.com/morytyann/OpenWrt-mihomo luci-app-mihomo mihomo
 # git clone https://github.com/kenzok8/jell --depth=1 package/small
 # git clone https://github.com/kenzok8/openwrt-packages --depth=1 package/small5
-git clone https://github.com/xiaorouji/openwrt-passwall-packages --depth=1 package/passwall-packages 
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages --depth=1 package/passwall-packages 
 git clone https://github.com/kenzok8/small-package --depth=1 package/small-package
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
